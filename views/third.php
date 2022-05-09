9bf3a56b13ffca7baabc138e4a08e1b7c8a0ffd4
zmadhar <zamadhar@gmail.com>
2022-05-09 00:40:32 -0400

    modified third part of the form

--- views/third.php
+++ views/third.php
@@ -7,6 +7,7 @@ project: creating a dating website
 
 
  --->
+
 <?php
 include 'Navbar.php';
 
@@ -28,10 +29,8 @@ if($_SERVER['REQUEST_METHOD']=='GET')
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css"
-          integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
     <link rel="stylesheet" href="../styles/third.css">
-    <title>Interests</title>
 
 </head>
 <style>
@@ -41,81 +40,34 @@ if($_SERVER['REQUEST_METHOD']=='GET')
 
 <hr>
 
-
-    <table  class="mytable">
-        <h1>In-door Interest</h1>
-
-     <form action="last.php" method="GET">
-
+<h1>Interest</h1>
+<table  class="mytable">
+    <h2>In-door Interest</h2>
+    <form action="last.php" method="GET">
 
 
 
-         <td><label>
-                 <input type="checkbox" name="info" value="TV">
-             </label>TV</input></td>
-         <td><label>
-                 <input type="checkbox" name="info" value="Movies">
-             </label>Movies</input></td>
+        <tr>
 
-         <td><label>
-                 <input type="checkbox" name="info" value="Cooking">
-             </label>Cooking</input></td>
-         <td><label>
-                 <input type="checkbox" name="info" value="Boardgame">
-             </label>Board Game</input></td>
-         <tr>
+            <td><input type="checkbox" name="info" value="TV">TV</input></td>
+            <td><input type="checkbox" name="info" value="Movies">Movies</input></td>
 
-      <td><label>
-              <input type="checkbox" name="info" value="Puzzels">
-          </label>Puzzels</input></td>
-        <td><label>
-                <input type="checkbox" name="info" value="Reading">
-            </label>Reading</input></td>
+            <td><input type="checkbox" name="info" value="Cooking">Cooking</input></td>
+            <td><input type="checkbox" name="info" value="Boardgame">Board Game</input></td>
+        </tr>
+        <tr>
+            <td><input type="checkbox" name="info" value="Puzzels">Puzzels</input></td>
+            <td><input type="checkbox" name="info" value="Reading">Reading</input></td>
 
-        <td><label>
-                <input type="checkbox" name="info" value="Playing Cards">
-            </label>Playing Cards</input></td>
-        <td><label>
-                <input type="checkbox" name="info" value="VideoGame">
-            </label>Video Game</input></td>
-    </tr>
+            <td><input type="checkbox" name="info" value="Playing Cards">Playing Cards</input></td>
+            <td><input type="checkbox" name="info" value="VideoGame">Video Game</input></td>
+        </tr>
 
-     <tr>
-         <title>out-door Interest</title>
-     </tr>
+        <tr>
+            <td colspan="4"><button type="submit">Next</button></td>
+        </tr>
 
-
-         <tr>
-             <td><label>
-                     <input type="checkbox" name="info" value="hiking">
-                 </label>hiking</input></td>
-             <td><label>
-                     <input type="checkbox" name="info" value="biking">
-                 </label>biking</input></td>
-
-             <td><label>
-                     <input type="checkbox" name="info" value="swimming">
-                 </label>swimming</input></td>
-             <td><label>
-                     <input type="checkbox" name="info" value="collecting">
-                 </label>collecting</input></td>
-         </tr>
-         <tr>
-             <td><label>
-                     <input type="checkbox" name="info" value="climbing">
-                 </label>climbing</input></td>
-             <td><label>
-                     <input type="checkbox" name="info" value="walking">
-                 </label>walking</input></td>
-
-         </tr>
-
-
-         <tr>
-        <td colspan="4"><button type="submit">Next</button></td>
-    </tr>
-
-</form>
-    </table>
+    </form>
+</table>
 </body>
 </html>
