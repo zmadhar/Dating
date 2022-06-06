5b13ee84ca9dfd7176c211252cc397a3235f4156
zmadhar <zamadhar@gmail.com>
2022-06-06 16:22:32 -0400

    fixed repo

--- index.php
+++ index.php
@@ -4,17 +4,17 @@
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
-session_start();
+
 
 //autoload file
 require_once('vendor/autoload.php');
-require_once('model/data-layer.php');
-require_once('model/validation.php');
-
+//require_once('model/data-layer.php');
+//require_once('model/validation.php');
+//require_once('controllers/controller.php');
 //Create an instance of the Base class
-
+session_start();
 $f3 = Base::instance();
-
+$controller = new Controller($f3);
 
 //Define a default route
 
@@ -26,190 +26,23 @@ $f3->route('GET|POST /', function(){
 
 $f3->route('GET|POST /first', function($f3){
 
-    if($_SERVER['REQUEST_METHOD'] == 'POST') {
+$GLOBALS["controller"]->first($f3);
 
-
-        $fname = $_POST['fname'];
-        $f3->set('fname', $fname);
-
-        $lname = $_POST['lname'];
-        $f3->set('lname', $lname);
-
-        $age = $_POST['age'];
-        $f3->set('age', $age);
-
-        $PhoneNumber = $_POST['PhoneNumber'];
-        $f3->set('phone', $PhoneNumber);
-
-        if (validFirst($fname)) {
-            $_SESSION['fname'] = $fname;
-        } else {
-            $f3->set('errors["fname"]', 'please enter a valid first name');
-        }
-
-
-        if (validLast($lname)) {
-            $_SESSION['lname'] = $lname;
-        } else {
-            $f3->set('errors["lname"]', 'Please enter a valid last name');
-        }
-
-
-        if (validAge($age)) {
-            $_SESSION['age'] = $age;
-        } else {
-            $f3->set('errors["age"]', 'Please enter an age above 18');
-        }
-
-
-        if (validPhone($PhoneNumber)) {
-            $_SESSION['PhoneNumber'] = $PhoneNumber;
-        } else {
-            $f3->set('errors["PhoneNumber"]', 'Please enter a valid phone number');
-        }
-        $gender= $_POST['gender'];
-        $f3->set('gender', $gender);
-        $gender = isset($_POST['gender']) ? $_POST['gender'] : "";
-        $_SESSION['gender'] = $gender;
-
-        if (empty($f3->get('errors'))) {
-            header('location: second');
-        }
-
-    }
-
-    $f3->set('gender', getGender());
-
-
-    $view = new Template();
-    echo $view->render('views/first.html');
 });
 
 
 
 $f3->route('GET|POST /second', function($f3){
 
+    $GLOBALS["controller"]->second($f3);
 
-    if  ($_SERVER['REQUEST_METHOD'] == 'POST'){
-
-        $email = $_POST['email'];
-        $f3->set('email', $email);
-
-        //if seeking is set
-        $seeking = isset($_POST['seeking']) ? $_POST['seeking'] : "";
-
-        //state
-        $state = $_POST['state'];
-
-
-        if (validEmail($email)){
-            $_SESSION['email'] = $email;
-        }
-
-        else{
-            $f3->set('errors["email"]', 'Please enter a valid email');
-        }
-
-
-        if (validSeeking($seeking)){
-            $_SESSION['seeking'] = $seeking;
-        }
-
-        else{
-            $f3->set('errors["seeking"]', 'Please select a valid option');
-        }
-
-        if (validState($state)){
-            $_SESSION['state'] = $state;
-        }
-
-        else{
-            $f3->set('errors["state"]', 'Please select a valid state');
-        }
-
-
-        $_SESSION['biography'] = $_POST['biography'];
-
-        //Redirect to profile route if there are no errors
-        if (empty($f3->get('errors'))) {
-            header('location: third');
-        }
-    }
-
-
-    $f3->set('seeking', getseeking());
-    $f3->set('state', getState());
-
-    $view = new Template();
-    echo $view->render('views/second.html');
 });
 
 
 $f3->route('GET|POST /third', function($f3){
 
+    $GLOBALS["controller"]->third($f3);
 
-    if  ($_SERVER['REQUEST_METHOD'] == 'POST'){
-
-        $indoor = [];
-        $f3->set('indoor', $indoor);
-
-        $outdoor = [];
-        $f3->set('outdoor', $outdoor);
-
-        //if seeking is set
-       if (isset($_POST['indoor'])){
-
-           $indoor= $_POST['indoor'];
-
-           $f3 ->set ('indoor',$indoor);
-       }
-        if (isset($_POST['outdoor'])){
-
-            $outdoor= $_POST['outdoor'];
-
-            $f3 ->set ('outdoor',$outdoor);
-        }
-
-        //state
-
-
-        if (validIndoor($indoor)|| empty($indoor)){
-            $_SESSION['indoor'] = $indoor;
-        }
-
-        else{
-            $f3->set('errors["indoor"]', 'Please select a valid option');
-        }
-
-
-        //if seeking is set
-        $outdoor = isset($_POST['outdoor']) ? $_POST['outdoor'] : "";
-
-        //state
-
-
-        if (validOutdoor($outdoor)|| empty($outdoor)){
-            $_SESSION['outdoor'] = $outdoor;
-        }
-
-        else{
-            $f3->set('errors["outdoor"]', 'Please select a valid option');
-        }
-
-
-
-        //Redirect to profile route if there are no errors
-        if (empty($f3->get('errors'))) {
-            header('location: last');
-        }
-    }
-
-
-    $f3->set('indoor', getIndoor());
-    $f3->set('outdoor', getOutdoor());
-
-    $view = new Template();
-    echo $view->render('views/third.html');
 });
 
 
@@ -226,6 +59,7 @@ $f3->route('GET|POST /last', function() {
 
 //fat free
 $f3->run();
+
 
 
 
--- model/validation.php
+++ model/validation.php
@@ -1,89 +1,98 @@
 <?php
 
-
 //validate firstname
-function validFirst($fname): bool
-{
 
-    return strlen(trim($fname))>=2;
-}
+class Validation
+
+{
+    static function validFirst($fname): bool
+    {
+
+        return strlen(trim($fname)) >= 2;
+    }
 
 
 //validate lastname
-function validLast($lname): bool
-{
+    static function validLast($lname): bool
+    {
 
-    return strlen(trim($lname))>=2;
-}
+        return strlen(trim($lname)) >= 2;
+    }
 
 //validate age
-function validAge($age){
-    return trim($age) >=18;
-}
+    static function validAge($age)
+    {
+        return trim($age) >= 18;
+    }
 
 // validate phone
-function validPhone($phone){
-    return filter_var($phone, FILTER_SANITIZE_NUMBER_INT);
-}
+    static function validPhone($phone)
+    {
+        return filter_var($phone, FILTER_SANITIZE_NUMBER_INT);
+    }
 
 
 // validate email
-function validEmail($email){
-    return !empty($email) && filter_var($email, FILTER_VALIDATE_EMAIL);
-}
-
-
-
-function validseeking($seeking){
-
-    if (in_array($seeking,getseeking())){
-
-        return true;
+    static function validEmail($email)
+    {
+        return !empty($email) && filter_var($email, FILTER_VALIDATE_EMAIL);
     }
 
 
-    return false;
+    static function validseeking($seeking)
+    {
+
+        if (in_array($seeking, getseeking())) {
+
+            return true;
+        }
 
 
-}
+        return false;
 
-function validState($state){
 
-    if (in_array($state,getState())){
-
-        return true;
     }
 
+    static function validState($state)
+    {
 
-    return false;
+        if (in_array($state, getState())) {
 
-}
+            return true;
+        }
 
-function validIndoor($indoor){
 
-    if (in_array($indoor,getIndoor())){
+        return false;
 
-        return true;
     }
 
+    static function validIndoor($indoor)
+    {
 
-    return false;
+        if (in_array($indoor, getIndoor())) {
 
-}
+            return true;
+        }
 
-function validOutdoor($outdoor){
 
-    if (in_array($outdoor,getOutdoor())){
+        return false;
 
-        return true;
     }
 
+    static function validOutdoor($outdoor)
+    {
 
-    return false;
+        if (in_array($outdoor, getOutdoor())) {
+
+            return true;
+        }
+
+
+        return false;
+
+    }
 
 }
-
-
 
 
 
